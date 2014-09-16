59c4325a73b4c62946b9b0b23a1675417b6fa3dc
Karthik Kottapalli <kartik.kottapalli@gmail.com>
2014-09-16 01:16:19 -0400

    Mocking with RSpec

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,13 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
   mount_uploader :image, ImageUploader
-  after_create :create_vote
 
   default_scope { order('rank DESC') }
   
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
-  # validates :topic, presence: true
-  # validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
   def up_votes
     votes.where(value: 1).count
@@ -31,8 +30,6 @@ class Post < ActiveRecord::Base
  
     update_attribute(:rank, new_rank)
   end
-
-  private
 
   def create_vote 
      user.votes.create(value: 1, post: self)
